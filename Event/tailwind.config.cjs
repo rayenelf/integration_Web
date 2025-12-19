830c0d3aa81b241fbdb70f4c070f351fe60e5af7
garoka87 <oussemasaidani147@gmail.com>
2025-12-20 00:58:43 +0100

    finale tailwind

---
+++ Event/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Event/*.html',
+    './Event/**/*.html',
+    './EventScss/**/*.{html,js,scss,css}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
