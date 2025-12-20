5d3fd4583c418eadfeafd2844caebeeadb5f0772
oussema ben el haj <126527296+oussemabb20@users.noreply.github.com>
2025-12-20 05:40:00 +0100

    fix

--- Event/tailwind.config.js
+++ Event/tailwind.config.js
@@ -2,10 +2,20 @@
 module.exports = {
   content: [
     "./*.html",
-    "./css/**/*.css"
+    "./public/**/*.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#B66C5A',
+        'primary-hover': '#9A5A4A',
+        secondary: '#4A3A3A',
+        'secondary-hover': '#3A2A2A',
+        sidebar: '#F5EBE8',
+        'sidebar-hover': '#E8D5D5',
+      }
+    }
   },
   plugins: []
 };
